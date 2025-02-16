781df6bccca022dfd54d8a72d65f12d7cf061715
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2025-02-16 11:16:43 +0100

    Still working on the UsersView

--- crond-ui-vaadin/src/main/java/com/github/jochenw/crond/vdnui/vdn/Filters.java
+++ crond-ui-vaadin/src/main/java/com/github/jochenw/crond/vdnui/vdn/Filters.java
@@ -1,7 +1,15 @@
 package com.github.jochenw.crond.vdnui.vdn;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
+import com.github.jochenw.afw.core.function.Predicates;
+import com.github.jochenw.afw.core.util.Objects;
+import com.github.jochenw.afw.core.util.Strings;
 import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.textfield.TextField;
 
@@ -40,6 +48,132 @@ public class Filters {
 		}
 	}
 
+	public static class ComparatorBuilder<O> {
+		private List<Comparator<O>> comparators;
+
+		public ComparatorBuilder<O> add(Comparator<O> pComparator) {
+			if (comparators == null) {
+				comparators = new ArrayList<>();
+			}
+			comparators.add(pComparator);
+			return this;
+		}
+
+		public <T> ComparatorBuilder<O> add(Function<O,T> pMapper, Comparator<T> pComparator) {
+			final Function<O,T> mapper = Objects.requireNonNull(pMapper, "Mapper");
+			final Comparator<T> comparator = Objects.requireNonNull(pComparator, "Comparator");
+			return add((o1,o2) -> {
+				return comparator.compare(mapper.apply(o1), mapper.apply(o2));
+			});
+		}
+
+		public ComparatorBuilder<O> add(Function<O,String> pMapper, boolean pAscending) {
+			final Function<O,String> mapper = Objects.requireNonNull(pMapper, "Mapper");
+			final BiFunction<Object,Object,Integer> nullChecker = (o1,o2) -> {
+				if (o1 == null) {
+					if (o2 == null) {
+						return Integer.valueOf(0);
+					} else {
+						return Integer.valueOf(-1);
+					}
+				} else {
+					if (o2 == null) {
+						return Integer.valueOf(1);
+					} else {
+						return null;
+					}
+				}
+			};
+			return add((o1, o2) -> {
+				if (!pAscending) { // Swap the objects, that are being compared.
+					final O o0 = o1;
+					o1 = o2;
+					o2 = o0;
+				}
+				Integer res = nullChecker.apply(o1, o2);
+				if (res == null) {
+				    final String s1 = mapper.apply(o1);	
+				    final String s2 = mapper.apply(o2);	
+				    res = nullChecker.apply(s1,  s2);
+				    if (res == null) {
+				    	if (pAscending) {
+				    		return s1.compareToIgnoreCase(s2);
+				    	} else {
+				    		return s2.compareToIgnoreCase(s1);
+				    	}
+				    }
+				}
+				return res.intValue();
+			});
+		}
+
+		public Comparator<O> build() {
+			if (comparators == null) {
+				return null;
+			} else if (comparators.size() == 1) {
+				return comparators.get(0);
+			} else {
+				return (o1,o2) -> {
+					for (int i = 0;  i < comparators.size();  i++) {
+						final int res = comparators.get(i).compare(o1, o2);
+						if (res != 0) {
+							return res;
+						}
+					}
+					return 0;
+				};
+			}
+		}
+	}
+	public static class PredicateBuilder<O> {
+		private List<Predicate<O>> predicates;
+
+		public PredicateBuilder<O> add(Predicate<O> pPredicate) {
+			if (predicates == null) {
+				predicates = new ArrayList<>();
+			}
+			predicates.add(pPredicate);
+			return this;
+		}
+
+		public PredicateBuilder<O> add(Function<O,String> pFunction, String pFilterString) {
+			final Predicate<String> stringPredicate = asStringPredicate(pFilterString);
+			if (stringPredicate != null) {
+				final Predicate<O> predicate = (o) -> {
+					if (o != null) {
+						final String v = pFunction.apply(o);
+						if (v != null) {
+							return stringPredicate.test(v);
+						}
+					}
+					return false;
+				};
+				return add(predicate);
+			}
+			return this;
+		}
+	
+		protected Predicate<String> asStringPredicate(String pPattern) {
+			final String pattern = Objects.requireNonNull(pPattern, "Pattern").replace('%', '*');
+			if (pattern.indexOf('*') >= 0  ||  pattern.indexOf('?') >= 0) {
+				return Strings.matcher(pattern);
+			} else {
+				final String patternLc = pattern.toLowerCase();
+				return (s) -> s.toLowerCase().contains(patternLc);
+			}
+		}
+
+		public Predicate<O> build() {
+			if (predicates == null  ||  predicates.isEmpty()) {
+				return Predicates.alwaysTrue();
+			} else if (predicates.size() == 1) {
+				return predicates.get(0);
+			} else {
+				return Predicates.allOf(predicates);
+			}
+		}
+	}
+
 	public static FilterStatusBuilder filterStatus() {
 		return new FilterStatusBuilder();
 	}
@@ -63,4 +197,20 @@ public class Filters {
 			}
 		};
 	}
-}
+
+	public static <O> PredicateBuilder<O> predicate() {
+		return new PredicateBuilder<>();
+	}
+
+	public static <O> PredicateBuilder<O> predicate(Class<O> pType) {
+		return new PredicateBuilder<>();
+	}
+
+	public static <O> ComparatorBuilder<O> comparator() {
+		return new ComparatorBuilder<>();
+	}
+
+	public static <O> ComparatorBuilder<O> comparator(Class<O> pTyoe) {
+		return new ComparatorBuilder<>();
+	}
+ }
--- crond-ui-vaadin/src/main/java/com/github/jochenw/crond/vdnui/vdn/UsersView.java
+++ crond-ui-vaadin/src/main/java/com/github/jochenw/crond/vdnui/vdn/UsersView.java
@@ -9,24 +9,28 @@ import java.util.stream.Stream;
 
 import com.github.jochenw.afw.core.util.MutableInteger;
 import com.github.jochenw.afw.core.util.Strings;
-import com.github.jochenw.afw.core.vdn.Filters.ComparatorBuilder;
-import com.github.jochenw.afw.core.vdn.Filters.PredicateBuilder;
 import com.github.jochenw.afw.di.api.IComponentFactory;
 import com.github.jochenw.crond.core.api.IModel;
 import com.github.jochenw.crond.core.api.IModel.User;
+import com.github.jochenw.crond.core.beans.UserImpl;
 import com.github.jochenw.crond.vdnui.MainView;
+import com.github.jochenw.crond.vdnui.vdn.Filters.ComparatorBuilder;
+import com.github.jochenw.crond.vdnui.vdn.Filters.PredicateBuilder;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.provider.CallbackDataProvider.CountCallback;
 import com.vaadin.flow.data.provider.CallbackDataProvider.FetchCallback;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.data.provider.QuerySortOrder;
+import com.vaadin.flow.data.provider.SortDirection;
 
 public class UsersView extends VerticalLayout {
 	private static final long serialVersionUID = 7296277703803132058L;
@@ -123,16 +127,42 @@ public class UsersView extends VerticalLayout {
 	}
 
 	protected void newUser() {
-		final Dialog dialog = new Dialog();
-		dialog.setModal(true);
-		final Button okButton = new Button("Okay");
-		final VerticalLayout vl = new VerticalLayout();
-		final NativeLabel label = new NativeLabel("Creating new user");
-		vl.add(label);
-		vl.add(okButton);
-		okButton.addClickListener((e) -> dialog.close());
-		dialog.add(vl);
-		dialog.open();
+		final User user = UserImpl.of(null, null, null);
+		editUser(user);
+	}
+
+	public static class EditableUser implements User {
+		private final Long id;
+		public EditableUser(Long pId) { id = pId; }
+		private String name, email;
+		@Override
+		public Long getId() { return id; }
+		@Override
+		public String getEmail() { return email; }
+		@Override
+		public String getName() { return name; }
+		public void setName(String pName) { name = pName; }
+		public void setEmail(String pEmail) { email = pEmail; }
+	}
+
+	protected void editUser(User pUser) {
+		final Long id = (pUser == null ||  pUser.getId() == null) ? null : pUser.getId();
+		final EditableUser eUser = new EditableUser(id);
+		if (pUser != null) {
+			eUser.setEmail(pUser.getEmail());
+			eUser.setName(pUser.getName());
+		}
+		final FormLayout fl = new FormLayout();
+		final Binder<EditableUser> binder = new Binder<>();
+		final TextField nameField = new TextField();
+		binder.forField(nameField)
+		    .withValidator((s) -> !Strings.isEmpty(s), "Name must not be empty")
+		    .bind(EditableUser::getName, EditableUser::setName);
+		fl.addFormItem(nameField, "Name");
+		final TextField emailField = new TextField();
+		binder.forField(emailField)
+		     .
+		fl.addFormItem(emailField, "Email");
 	}
 	
 	protected DataProvider<UiUser,Filter> newDataProvider() {
@@ -171,23 +201,11 @@ public class UsersView extends VerticalLayout {
 	}
 
 	final Predicate<User> asPredicate(Query<UiUser,Filter> pQuery) {
-		final PredicateBuilder<User> pb = com.github.jochenw.afw.core.vdn.Filters.predicate(User.class);
-		final String filterIdStr = filter.id == null ? null : filter.id.trim();
-		if (filterIdStr != null  &&  filterIdStr.length() > 0) {
-			final Predicate<String> idPredicate = Strings.matcher(filterIdStr.replace('%', '*'));
-			pb.add((u) -> u.getId().toString(), idPredicate);
-		}
-		final String filterNameStr = filter.name == null ? null : filter.name.trim();
-		if (filterNameStr != null  &&  filterNameStr.length() > 0) {
-			final Predicate<String> namePredicate = Strings.matcher(filterNameStr.replace('%', '?'));
-			pb.add(User::getName, namePredicate);
-		}
-		final String filterEmailStr = filter.email == null ? null : filter.email.trim();
-		if (filterEmailStr != null  &&  filterEmailStr.length() > 0) {
-			final Predicate<String> emailPredicate = Strings.matcher(filterEmailStr.replace('%', '*'));
-			pb.add(User::getEmail, emailPredicate);
-		}
-		return pb.build();
+	    final PredicateBuilder<User> pb =  Filters.predicate();
+	    return pb.add((User u) -> u.getId().toString(), filter.id)
+	    		 .add(User::getName, filter.name)
+	    		 .add(User::getEmail, filter.email)
+	    		 .build();
 	}
 
 	final Comparator<User> asComparator(Query<UiUser,Filter> pQuery) {
@@ -195,39 +213,26 @@ public class UsersView extends VerticalLayout {
 		if (list == null  ||  list.isEmpty()) {
 			return null;
 		}
-		final ComparatorBuilder<User> cb = com.github.jochenw.afw.core.vdn.Filters.comparator();
+		final ComparatorBuilder<User> cb = Filters.comparator();
+		
 		list.forEach((qso) -> {
-			final Function<User,Object> getter;
-			final Comparator<Object> comparator;
+			final Function<User,String> getter;
 			switch (qso.getSorted()) {
 			case "id": {
-				    getter = (u) -> u.getId();
-				    comparator = (o1,o2) -> {
-				    	return ((Long) o1).compareTo((Long) o2);
-				    };
-			    }
+					getter = (u) -> u.getId().toString();
+				}
 				break;
 			case "name": {
-				    getter = User::getName;
-				    comparator = (o1,o2) -> {
-				    	return compareStrings((String) o1, (String) o2);
-				    };
-			    }
+					getter = User::getName;
+				}
 				break;
 			case "email": {
 				    getter = User::getEmail;
-				    comparator = (o1,o2) -> {
-				    	return compareStrings((String) o1, (String) o2);
-				    };
 			    }
 			    break;
 			default: throw new IllegalStateException("Invalid filter property: " + qso.getSorted());
 			}
-			switch (qso.getDirection()) {
-			case ASCENDING: cb.add(getter, comparator);
-			case DESCENDING: cb.add(getter, comparator.reversed());
-			default: throw new IllegalStateException("Invalid filter direction: " + qso.getDirection());
-			}
+			cb.add(getter, qso.getDirection() == SortDirection.ASCENDING);
 		});
 		return cb.build();
 	}
